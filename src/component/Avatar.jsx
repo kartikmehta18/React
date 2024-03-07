f1b5d3c1b47c19fd2d4d4d103d21343523a66973
kartikmehta18 <125860170+kartikmehta18@users.noreply.github.com>
2024-03-07 20:18:43 +0530

    hi

---
+++ src/component/Avatar.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Avatar =() => {
+    return <img src={Avatar URL} className="rounded-full- h-48"/>
+    
+};
+export default Avatar;
--- src/component/profilcard.jsx
+++ src/component/profilcard.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import ProfilCard from "./component/profilcard";
+import Avatar from "./Avatar";
 
 const ProfileCard = () => {
     return (
-        <div className="bg-white">
+        <div className="br-white">
+        <Avatar avtarURL/>
+
         </div>
-           );
+    )   
     }
